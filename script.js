8db73605e2dacb6ccb977d681908f1d9fd7037b5
Caleb Asher <caleb@lytemine.com>
2016-02-29 12:04:50 -0700

    adds ability to choose team

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
 
+	var you;
+	var computer;
+	var game = function() {
+		$('.box').click(function(){
+			$(this).html('<p>'+you+'</p>')
+		})
+	};	
+
+	//modal code
+
+	$('#newGame').click(function(){ 
+		$("#myModal").modal();
+	})
+
+	$('#chooseX').click(function(){
+		$('.box').empty();
+		you = "X";
+		computer = "O";
+		game();
+	})
+
+	$('#chooseO').click(function(){
+		$('.box').empty();
+		you = "O";
+		computer = "X";
+		game();
+	})
+
 });
