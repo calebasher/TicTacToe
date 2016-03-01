9f182759d56e679bf43c0b3dd214ca8e5bdb8349
Caleb Asher <caleb@lytemine.com>
2016-03-01 12:09:46 -0700

    finished functional version with simple AI and no sound effects.

--- script.js
+++ script.js
@@ -1,12 +1,95 @@
 $(document).ready(function(){
 
-	var you;
-	var computer;
-	var game = function() {
-		$('.box').click(function(){
-			$(this).html('<p>'+you+'</p>')
-		})
-	};	
+	var yourIcon;
+	var computerIcon;
+	var yourTurn;
+	var computerChoice;
+	var won=0;
+	var lost =0;
+	var tied = 0;
+	var moves = 0;
+	var winnerFound = false;
+
+	function updateScore(){
+		$('#won').html("Won: "+won);
+		$('#lost').html("Lost: "+lost);
+		$('#tied').html("Tied: "+tied);
+	}
+
+	function whoStarts() {
+		var number = Math.random();
+		number > 0.5 ? yourTurn = true : yourTurn = false;
+	}
+
+	function startGame() {
+		if (yourTurn !== true) {	
+			computerGoes();
+		}
+	}
+
+    $('.box').click(function(){
+    	if (yourIcon !== undefined && $('#status').is(':empty')) {
+	        $(this).html('<p>'+yourIcon+'</p>');
+	        moves ++; 
+	        checkForWinner(yourIcon);
+	        computerGoes();
+	    }
+    })        
+
+    function computerGoes(){
+            computerChoice =  Math.floor((Math.random() * 9) + 1);
+            if ($('#box'+computerChoice+'').is(':empty')) {
+                $('#box'+computerChoice+'').html('<p>'+computerIcon+'</p>');
+                moves ++;
+                checkForWinner(computerIcon);   
+            }
+            else if ($('#box'+computerChoice+'').is(':empty') === false && moves == 9) {            
+                checkForWinner(yourIcon);
+            }  
+            else if ($('#box'+computerChoice+'').is(':empty') === false) {            
+                computerGoes();
+            } 
+                           
+    }
+
+    function flash(){
+    	$('#box'+winningArray[i][0]+'').effect("pulsate", { times:3 }, 2000);
+		$('#box'+winningArray[i][1]+'').effect("pulsate", { times:3 }, 2000);
+		$('#box'+winningArray[i][2]+'').effect("pulsate", { times:3 }, 2000);
+    }
+
+    function checkForWinner(icon) {
+		var currentIcon = icon;
+		winningArray = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+
+			for (i=0; i<winningArray.length; i++) {
+				if ($('#box'+winningArray[i][0]+'').text() == currentIcon && 
+					$('#box'+winningArray[i][1]+'').text() == currentIcon && 
+					$('#box'+winningArray[i][2]+'').text() == currentIcon) {
+
+					if (icon == computerIcon) {
+						flash();
+						$('#status').hide().html('You Lost').fadeIn(2500);
+						lost ++;
+						updateScore();
+						winnerFound = true;
+					}
+					else if (icon == yourIcon) {
+						flash();
+						$('#status').hide().html('You Won!').fadeIn(2500);
+						won ++;
+						updateScore();
+						winnerFound = true;
+					}
+				}
+				else if (moves == 9 && winnerFound === false) {
+						$('#status').hide().html('It\'s a Tie').fadeIn(2500);
+						tied++;
+						updateScore();
+						winnerFound = true;
+					}	
+			}
+	}
 
 	//modal code
 
@@ -16,16 +99,24 @@ $(document).ready(function(){
 
 	$('#chooseX').click(function(){
 		$('.box').empty();
-		you = "X";
-		computer = "O";
-		game();
+		$('#status').empty();
+		winnerFound = false;
+		yourIcon = "X";
+		computerIcon = "O";
+		moves = 0;
+		whoStarts();
+		startGame();	
 	})
 
 	$('#chooseO').click(function(){
 		$('.box').empty();
-		you = "O";
-		computer = "X";
-		game();
+		$('#status').empty();
+		winnerFound = false;
+		yourIcon = "O";
+		computerIcon = "X";
+		moves = 0;
+		whoStarts();
+		startGame();	
 	})
 
 });
